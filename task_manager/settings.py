c809cf5bf17b16344d80e8199a44945c9e929e2b
Андрей Трушин <trand1@mail.ru>
2022-07-10 16:23:09 +0700

    10_07_22

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -19,10 +19,6 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Include BOOTSTRAP4_FOLDER in path
-BOOTSTRAP4_FOLDER = os.path.abspath(os.path.join(BASE_DIR, "..", "bootstrap4"))
-if BOOTSTRAP4_FOLDER not in sys.path:
-    sys.path.insert(0, BOOTSTRAP4_FOLDER)
 
 
 # Quick-start development settings - unsuitable for production
@@ -158,18 +154,6 @@ STATICFILES_FINDERS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-BOOTSTRAP4 = {
-    "css_url": {
-        "href": "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css",
-        "integrity": "sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l",
-        "crossorigin": "anonymous",
-    },
-    "javascript_url": {
-        "url": "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js",
-        "integrity": "sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns",
-        "crossorigin": "anonymous",
-    },
-},
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
